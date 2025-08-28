7e937128a81203c046de39dbbc97a0831eff4279
ConstantSeal592 <reubensmith@outlook.com>
2025-08-28 16:59:29 +0100

    widgets, and alot of them

--- myproject/members/views.py
+++ myproject/members/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 from django.template import loader
 from . import models
 
+def getdb():
+    return models.member.objects.all().values()
+
+def GetWidgets():
+    membersdb = getdb()
+    context = {
+        'membersdb': membersdb,
+        'data': {
+            'x': 1,
+            'y': 5,
+            'width': 2,
+            'height': 2
+        }
+    }
+    
+    html = loader.render_to_string("members_widget.html", context)
+    return [{'type': 'string', 'html': html}]
+
 def members(request):
-    membersdb = models.member.objects.all().values()
+    membersdb = getdb()
 
     template = loader.get_template("members.html")
     context = {
